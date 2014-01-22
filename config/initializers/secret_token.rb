976e924d81befa2b6545d38067cc211901886642
KYN 2 <KYN2@kyns-air-2.gateway.2wire.net>
2014-01-21 22:31:03 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Treebook::Application.config.secret_key_base = '19010de24edead7e25eb1e821f8f1bdfd3ef2b8ef0cb4f50395b0b0dbc61e84c4374270eb93d51e0a7cf17b7a8655b4eedbb1afd3a435ffe96dc7f64ef419c91'
