0302027b7d7d0173d2e67934916f2e9d6ad40ddb
Taylor Mitchell <scy0846@yahoo.com>
2014-01-26 23:43:43 -0500

    set up profile pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Treebook::Application.routes.draw do
+  get "profiles/show"
   devise_for :users
   
   devise_scope :user do
@@ -12,6 +13,8 @@ Treebook::Application.routes.draw do
   root to: 'statuses#index'
 
 
+
+  get '/:id', to: 'profiles#show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
